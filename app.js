e5af2ff9666e66fbbe378b05b7da3d57491a11fc
Akansh Garg <akansh.garg@nagarro.com>
2024-05-30 12:50:14 +0530

    commit for testing mongo connection string

--- app.js
+++ app.js
@@ -1,11 +1,179 @@
 var express = require('express');
 var bodyParser = require('body-parser');
-var mongoose = require('mongoose');
-
+var mongoose1 = require('mongoose');
+var mongoose2 = require('mongoose');
+var mongoose3 = require('mongoose');
 
 var app = express();
 
 var port = process.env.PORT || 3000;
+
+
+/***********TEST MONGO CONNECTION CODE ********* */
+
+let MongoClient1 = require('mongodb').MongoClient;
+let MongoClient2 = require('mongodb').MongoClient;
+let MongoClient3 = require('mongodb').MongoClient;
+
+try {
+
+    MongoClient1.connect("mongodb://mongouser:mongopassword@mongo-0.mongo.default.svc.cluster.local:27017/todos", function (err, db) {
+
+        if (err) {
+            console.log(err)
+            throw err;
+        }
+
+        console.log("I CAN CONNECT Connection 1 !!")
+
+    });
+
+}
+catch (e) {
+
+    console.log("Error Connection 1 !!")
+    console.log(e);
+
+}
+
+
+try {
+
+    MongoClient2.connect("mongodb://mongouser:mongopassword@mongo.default.svc.cluster.local:27017/todos", function (err, db) {
+
+        if (err) {
+            console.log(err)
+            throw err;
+        }
+
+        console.log("I CAN CONNECT Connection 2 !!")
+
+    });
+
+}
+catch (e) {
+
+    console.log("Error Connection 2 !!")
+
+    console.log(e);
+
+}
+
+
+try {
+
+    MongoClient3.connect("mongodb+srv://mongouser:mongopassword@mongo.default.svc.cluster.local:27017/todos", function (err, db) {
+
+        if (err) {
+            console.log(err)
+            throw err;
+        }
+
+        console.log("I CAN CONNECT Connection 3 !!")
+
+    });
+
+}
+catch (e) {
+
+    console.log("Error Connection 3 !!")
+
+    console.log(e);
+
+}
+
+
+try {
+    const mongoUrlK8sheadlessService = `mongodb://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@${process.env.MONGO_HOSTNAME}:${process.env.MONGO_PORT}/${process.env.MONGO_DB}?replicaSet=${process.env.MONGO_REPLICASET}&authSource=admin`;
+
+    // let mongoUrlK8sheadlessService = `mongodb://${process.env.USER_NAME}:${process.env.USER_PWD}@${process.env.DB_URL}/${process.env.Database_Name}?replicaSet=${process.env.replSetName},directConnection=true;readPreference=PrimaryPreferred`;
+
+    console.log('mongoUrlK8sheadlessService =' + mongoUrlK8sheadlessService);
+
+
+
+    //db connection with mongoose(mongodb)
+    mongoose1.connect(mongoUrlK8sheadlessService, {
+        // dbName:${process.env.MONGO_DB},
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    });
+
+    console.log("I CAN CONNECT Connection 4 !!")
+
+}
+catch (e) {
+
+    console.log("Error Connection 4 !!")
+
+    console.log(e);
+
+}
+
+
+try {
+    let mongoUrlK8sheadlessService5 = `mongodb://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@${process.env.MONGO_HOSTNAME}:${process.env.MONGO_PORT}/${process.env.MONGO_DB}?retryWrites=true&w=majority`;
+
+    // let mongoUrlK8sheadlessService = `mongodb://${process.env.USER_NAME}:${process.env.USER_PWD}@${process.env.DB_URL}/${process.env.Database_Name}?replicaSet=${process.env.replSetName},directConnection=true;readPreference=PrimaryPreferred`;
+
+    console.log('mongoUrlK8sheadlessService =' + mongoUrlK8sheadlessService5);
+
+
+
+    //db connection with mongoose(mongodb)
+    mongoose2.connect(mongoUrlK8sheadlessService5, {
+        // dbName:${process.env.MONGO_DB},
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    });
+
+    console.log("I CAN CONNECT Connection 5 !!")
+
+}
+catch (e) {
+
+    console.log("Error Connection 5 !!")
+
+    console.log(e);
+
+}
+
+
+
+try {
+    let mongoUrlK8sheadlessService6 = `mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@${process.env.MONGO_HOSTNAME}:${process.env.MONGO_PORT}/${process.env.MONGO_DB}?retryWrites=true&w=majority`;
+
+    // let mongoUrlK8sheadlessService = `mongodb://${process.env.USER_NAME}:${process.env.USER_PWD}@${process.env.DB_URL}/${process.env.Database_Name}?replicaSet=${process.env.replSetName},directConnection=true;readPreference=PrimaryPreferred`;
+
+    console.log('mongoUrlK8sheadlessService =' + mongoUrlK8sheadlessService6);
+
+
+
+    //db connection with mongoose(mongodb)
+    mongoose3.connect(mongoUrlK8sheadlessService6, {
+        // dbName:${process.env.MONGO_DB},
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    });
+
+    console.log("I CAN CONNECT Connection 6 !!")
+
+}
+catch (e) {
+
+    console.log("Error Connection 6 !!")
+
+    console.log(e);
+
+}
+
+
+
+/************************************************** */
+
+
+
+
 
 //"mongodb://user:password@mongodb-cluster:27000/dbname?replicaSet=&amp;authSource=$external&amp;authMechanism=PLAIN&amp;ssl=true&amp;sslVerifyCertificate=false&amp;maxPoolSize=5&amp;waitQueueMultiple=20&amp;readPreference=PrimaryPreferred" />
 
@@ -26,35 +194,35 @@ var port = process.env.PORT || 3000;
 // //     console.log(error);
 // //   }
 
- // headless service - mongodb://user:pwd@mongodb-headless.svc.cluster.local:27017/dbname_?authSource=admin&replicaSet=rs0
+// headless service - mongodb://user:pwd@mongodb-headless.svc.cluster.local:27017/dbname_?authSource=admin&replicaSet=rs0
 
- // Create the connection string with all replica set members
- //const uri = `mongodb://${username}:${password}@mongo-0.mongo-svc.default.svc.cluster.local:27017/${database}?replicaSet=${replicaSetName},directConnection=true`;
+// Create the connection string with all replica set members
+//const uri = `mongodb://${username}:${password}@mongo-0.mongo-svc.default.svc.cluster.local:27017/${database}?replicaSet=${replicaSetName},directConnection=true`;
 
- // Use the StatefulSet service name to reach any MongoDB pod
+// Use the StatefulSet service name to reach any MongoDB pod
 // const fqdn = 'mongo-0.mongo.default.svc.cluster.local:27017';
 // const port = '27017';
-const databasename =process.env.MONGO_DB;
+const databasename = process.env.MONGO_DB;
 // MongoDB StatefulSet replica set name
-    //const replicaSetName = 'rs0';
+//const replicaSetName = 'rs0';
 
 
 
- const mongoUrlK8sheadlessService = `mongodb://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@${process.env.MONGO_HOSTNAME}:${process.env.MONGO_PORT}/${process.env.MONGO_DB}?replicaSet=${process.env.MONGO_REPLICASET}&authSource=admin`;
+// //  const mongoUrlK8sheadlessService = `mongodb://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@${process.env.MONGO_HOSTNAME}:${process.env.MONGO_PORT}/${process.env.MONGO_DB}?replicaSet=${process.env.MONGO_REPLICASET}&authSource=admin`;
 
-// let mongoUrlK8sheadlessService = `mongodb://${process.env.USER_NAME}:${process.env.USER_PWD}@${process.env.DB_URL}/${process.env.Database_Name}?replicaSet=${process.env.replSetName},directConnection=true;readPreference=PrimaryPreferred`;
+// // // let mongoUrlK8sheadlessService = `mongodb://${process.env.USER_NAME}:${process.env.USER_PWD}@${process.env.DB_URL}/${process.env.Database_Name}?replicaSet=${process.env.replSetName},directConnection=true;readPreference=PrimaryPreferred`;
 
-console.log ('mongoUrlK8sheadlessService =' + mongoUrlK8sheadlessService);
+// // console.log ('mongoUrlK8sheadlessService =' + mongoUrlK8sheadlessService);
 
 // use when starting application locally with node command
 //let mongoUrlLocal = "mongodb://admin:password@localhost:27017";
 
 //db connection with mongoose(mongodb)
- mongoose.connect(mongoUrlK8sheadlessService, {
-    // dbName:${process.env.MONGO_DB},
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-});
+// //  mongoose.connect(mongoUrlK8sheadlessService, {
+// //     // dbName:${process.env.MONGO_DB},
+// //     useNewUrlParser: true,
+// //     useUnifiedTopology: true
+// // });
 
 
 // // // mongoose.connect("mongodb://localhost/todo", {
@@ -68,7 +236,7 @@ app.use(express.static(__dirname + '/public'));
 
 //interact with index.ejs
 app.set("view engine", "ejs");
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 //mongoose schema
 var todoSchema = new mongoose.Schema({
@@ -110,80 +278,80 @@ var Todo = mongoose.model("Todo", todoSchema);
 
 
 //routes
-app.get("/", (req, res)=>{
-    Todo.find({}, (error, todoList)=>{
-        if(error){
-            console.log(error);
-        }
-        else{
-            res.render("index.ejs", {todoList: todoList});
-        }
-    });
-});
+// // app.get("/", (req, res)=>{
+// //     Todo.find({}, (error, todoList)=>{
+// //         if(error){
+// //             console.log(error);
+// //         }
+// //         else{
+// //             res.render("index.ejs", {todoList: todoList});
+// //         }
+// //     });
+// // });
 
 
 //route for adding new task
-app.post("/newtodo", (req, res)=>{
-    var newTask = new Todo({
-        name: req.body.task
-    });
+// // app.post("/newtodo", (req, res)=>{
+// //     var newTask = new Todo({
+// //         name: req.body.task
+// //     });
 
-console.log(newTask);
+// // console.log(newTask);
 
-    //add to db
-    Todo.create(newTask, (err, Todo)=>{
-        if(err){
-            console.log(err);
-        }
-        else{
-            console.log(`inserted ${newTask} to the database todo`);
-            res.redirect("/");
-        }
-    });
-});
+// //     //add to db
+// //     Todo.create(newTask, (err, Todo)=>{
+// //         if(err){
+// //             console.log(err);
+// //         }
+// //         else{
+// //             console.log(`inserted ${newTask} to the database todo`);
+// //             res.redirect("/");
+// //         }
+// //     });
+// // });
 
 //route to delete a task by id
-app.get("/delete/:id", (req, res)=>{
-    var taskId = req.params.id;//get the id from the api 
-    console.log(req.params.id);
-    mongoose.model('Todo').deleteOne({_id: taskId}, (err, result)=>{
-        if(err){
-            console.log(`Error is deleting the task ${taskId}`);
-        }
-        else{
-            console.log("Task successfully deleted from database");
-            res.redirect("/");
-        }
-    });
-});
+// // app.get("/delete/:id", (req, res)=>{
+// //     var taskId = req.params.id;//get the id from the api 
+// //     console.log(req.params.id);
+// //     mongoose.model('Todo').deleteOne({_id: taskId}, (err, result)=>{
+// //         if(err){
+// //             console.log(`Error is deleting the task ${taskId}`);
+// //         }
+// //         else{
+// //             console.log("Task successfully deleted from database");
+// //             res.redirect("/");
+// //         }
+// //     });
+// // });
 
 //route for deleting all tasks
-app.post("/delAlltodo", (req, res)=>{
-    var myquery = { name: /^O/ };
-    mongoose.model('Todo').deleteMany({}, (err, result)=>{
-        if(err){
-            console.log(err);
-        }
-        else{
-            console.log(`Deleted all tasks`);
-            res.redirect("/");
-        }
-    });
-});
+// // app.post("/delAlltodo", (req, res)=>{
+// //     var myquery = { name: /^O/ };
+// //     mongoose.model('Todo').deleteMany({}, (err, result)=>{
+// //         if(err){
+// //             console.log(err);
+// //         }
+// //         else{
+// //             console.log(`Deleted all tasks`);
+// //             res.redirect("/");
+// //         }
+// //     });
+// // });
 
 //catch the invalid get requests
-app.get("*", (req, res)=>{
+app.get("*", (req, res) => {
     res.send("<h1>Invalid Page</h1>");
 });
 
 //listen on port 3000
-app.listen(port, (error)=>{
-    if(error){
-        console.log("Issue in connecting to the server ");
+app.listen(port, (error) => {
+    if (error) {
+        console.log("Issue in connecting to the server " + error);
 
         //http://localhost:3000/
     }
-    else{
+    else {
         console.log("Successfully connected to the server . server running on port 3000 . http://localhost:3000/");
     }
 })
